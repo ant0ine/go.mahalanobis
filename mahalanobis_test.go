e5cb4ecea2a02d91dcef00ceda4874b03a7836b7
antoine <antoine.imbert@gmail.com>
2012-10-20 06:16:32 +0000

    don't use ParseMatlab

--- mahalanobis_test.go
+++ mahalanobis_test.go
@@ -9,31 +9,27 @@ import (
 
 func TestMeanVector(t *testing.T) {
 
-	points, err := matrix.ParseMatlab("[1 1 1;1 1 1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected, err := matrix.ParseMatlab("[1;1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	points := matrix.MakeDenseMatrix([]float64{
+		1, 1, 1,
+		1, 1, 1,
+	}, 2, 3)
+	expected := matrix.MakeDenseMatrix([]float64{
+		1,
+		1,
+	}, 2, 1)
 	result := MeanVector(points)
 	if !matrix.Equals(result, expected) {
 		t.Error()
 	}
 
-	points, err = matrix.ParseMatlab("[0 1 2;0 2 4]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected, err = matrix.ParseMatlab("[1;2]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	points = matrix.MakeDenseMatrix([]float64{
+		0, 1, 2,
+		0, 2, 4,
+	}, 2, 3)
+	expected = matrix.MakeDenseMatrix([]float64{
+		1,
+		2,
+	}, 2, 1)
 	result = MeanVector(points)
 	if !matrix.Equals(result, expected) {
 		t.Error()
@@ -44,16 +40,14 @@ func TestCovarianceMatrix(t *testing.T) {
 
 	// no (co)variance
 	// R: var(cbind(c(1, 1), c(1, 1)))
-	points, err := matrix.ParseMatlab("[1 1;1 1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected, err := matrix.ParseMatlab("[0 0;0 0]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	points := matrix.MakeDenseMatrix([]float64{
+		1, 1,
+		1, 1,
+	}, 2, 2)
+	expected := matrix.MakeDenseMatrix([]float64{
+		0, 0,
+		0, 0,
+	}, 2, 2)
 	result := CovarianceMatrix(points)
 	//fmt.Println("covariance:\n", result)
 	if !matrix.Equals(result, expected) {
@@ -62,14 +56,14 @@ func TestCovarianceMatrix(t *testing.T) {
 
 	// diagonale case
 	// R: var(cbind(c(0, 4, 2, 2), c(2, 2, 0, 4)))
-	points, err = matrix.ParseMatlab("[0 4 2 2;2 2 0 4]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// R: var(cbind(c(0, 4, 2, 2), c(2, 2, 0, 4)))
-	expected = matrix.MakeDenseMatrix([]float64{2.66, 0, 0, 2.66}, 2, 2)
-
+	points = matrix.MakeDenseMatrix([]float64{
+		0, 4, 2, 2,
+		2, 2, 0, 4,
+	}, 2, 4)
+	expected = matrix.MakeDenseMatrix([]float64{
+		2.66, 0,
+		0, 2.66,
+	}, 2, 2)
 	result = CovarianceMatrix(points)
 	//fmt.Println("covariance:\n", result)
 	if !matrix.ApproxEquals(result, expected, 0.01) {
@@ -78,13 +72,14 @@ func TestCovarianceMatrix(t *testing.T) {
 
 	// another case
 	// R: var(cbind(c(9, 3, 5), c(3, 4, 1)))
-	points, err = matrix.ParseMatlab("[9 3 5;3 4 1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected = matrix.MakeDenseMatrix([]float64{9.33, -0.66, -0.66, 2.33}, 2, 2)
-
+	points = matrix.MakeDenseMatrix([]float64{
+		9, 3, 5,
+		3, 4, 1,
+	}, 2, 3)
+	expected = matrix.MakeDenseMatrix([]float64{
+		9.33, -0.66,
+		-0.66, 2.33,
+	}, 2, 2)
 	result = CovarianceMatrix(points)
 	//fmt.Println("covariance:\n", result)
 	if !matrix.ApproxEquals(result, expected, 0.01) {
@@ -97,22 +92,22 @@ func TestDistance(t *testing.T) {
 	// R:
 	// x = cbind(c(9, 3, 5), c(3, 4, 1))
 	// mahalanobis(c(1,1), colMeans(x), var(x))
-	points, err := matrix.ParseMatlab("[9 3 5;3 4 1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	target, err := matrix.ParseMatlab("[1;1]")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	points := matrix.MakeDenseMatrix([]float64{
+		9, 3, 5,
+		3, 4, 1,
+	}, 2, 3)
+	target := matrix.MakeDenseMatrix([]float64{
+		1,
+		1,
+	}, 2, 1)
 	square, err := DistanceSquare(points, target)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if math.Abs(square-4.08) > 0.01 {
+	var expected float64
+	expected = 4.08
+	// is near
+	if math.Abs(square-expected) > 0.01 {
 		t.Error()
 	}
 }
